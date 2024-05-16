194c3484bbee308e0826726bd12ccd22623ee5b2
DavidSheltonSF <david.faria012@gmail.com>
2024-05-16 00:35:15 -0300

    test(entities/validators): Implementing new tests for Name

--- src/entities/validators/tests/name.spec.ts
+++ src/entities/validators/tests/name.spec.ts
@@ -5,7 +5,23 @@ describe('Name validator', ()=> {
     expect(Name.validate('David')).toBe(true);
   });
   
-  test('Should not accept name with special caracteres', () => {
+  test('Should not accept name with any special characters', () => {
     expect(Name.validate('David@')).toBe(false);
-  })
+  });
+
+  test('Should not accept name with any numbers', () => {
+    expect(Name.validate('David1')).toBe(false);
+  });
+
+  test('Should not accept name with just one character', () => {
+    expect(Name.validate('D')).toBe(false);
+  });
+
+  test('Should not accept name with more than 255 characters', () => {
+    let invalidName = '';
+    for (let i = 0; i < 256; i++){
+      invalidName += 'D';
+    }
+    expect(Name.validate(invalidName)).toBe(false);
+  });
 })
