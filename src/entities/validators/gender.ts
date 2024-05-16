c797eca86f9b9032b4a525d6a290ae1cf0d76482
DavidSheltonSF <david.faria012@gmail.com>
2024-05-16 15:44:37 -0300

    feat(entities/valitators): Implementing gender validator

---
+++ src/entities/validators/gender.ts
@@ -0,0 +1,44 @@
+import { Either, left, right } from "../../shared/either";
+import { InvalidGenderError } from "../errors/invalid-gender";
+
+enum GenderEnum{
+  M = 'male',
+  F = 'female'
+}
+
+export class Gender {
+  private readonly gender: string;
+
+  private constructor (gender: string){
+    this.gender = gender;
+    Object.freeze(this);
+  }
+
+  get value (): string{
+    return this.gender;
+  }
+
+  static validate(gender: string): boolean {
+
+    switch(gender.toLocaleLowerCase()){
+
+      case GenderEnum.M:
+        return true;
+      
+      case GenderEnum.F:
+        return true;
+      
+      default:
+        return false;
+    }
+  }
+
+  static create(gender: string): Either<InvalidGenderError, Gender> {
+    
+    if (!Gender.validate(gender)){
+      return left(new InvalidGenderError(gender))
+    }
+
+    return right(new Gender(gender));
+  }
+}
