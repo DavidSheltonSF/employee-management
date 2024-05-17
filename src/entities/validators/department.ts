996f7857b432763e1c5fd3f2fcdcd6792645aa49
DavidSheltonSF <david.faria012@gmail.com>
2024-05-16 22:22:12 -0300

    feat(entities/validators): Implementing Department validator

---
+++ src/entities/validators/department.ts
@@ -0,0 +1,42 @@
+import { Either, left, right } from "../../shared/either";
+import { InvalidDepartmentError } from "../errors/invalid-department";
+
+enum DepartmentEnum {
+  CUSTUMER_SERVICE = 'custumerService',
+  ADMINISTRATIVE = 'administrative',
+  CLEANING = 'cleaning'
+}
+
+export class Department {
+  private readonly department: string
+
+  private constructor(department: string){
+    this.department = department;
+  }
+
+  get value (): string{
+    return this.department;
+  }
+
+  static validate(department: string): boolean{
+    const departmentList = Object.values(DepartmentEnum)
+      .map((elem) => {
+        return String(elem);
+      });
+
+    if(!(departmentList.includes(department))){
+      return false;
+    }
+
+    return true;
+  }
+
+  static create(department: string): Either<InvalidDepartmentError, Department>{
+    
+    if (!Department.validate(department)){
+      return left(new InvalidDepartmentError(department));
+    }
+
+    return right(new Department(department))
+  }
+}
