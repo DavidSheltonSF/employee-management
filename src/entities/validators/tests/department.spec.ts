82efbbb104e4f510feeb02a9d63c4452715dd73d
DavidSheltonSF <david.faria012@gmail.com>
2024-05-16 23:16:36 -0300

    test(entities/validators): Testing Department validator

---
+++ src/entities/validators/tests/department.spec.ts
@@ -0,0 +1,18 @@
+import { Department } from "../department";
+
+describe('Department validator', () => {
+  test('Should accept valid department (valid classes)', () => {
+    expect(Department.validate('customer_service')).toBe(true);
+    expect(Department.validate('administrative')).toBe(true);
+    expect(Department.validate('cleaning')).toBe(true);
+  });
+
+  test('Should accept valid department values regardless if the value is lowercase, uppercase, etc', () => {
+    expect(Department.validate('CUSTOMER_SERVICE')).toBe(true);
+    expect(Department.validate('ADMINISTRATIVE')).toBe(true);
+    expect(Department.validate('CLEANING')).toBe(true);
+    expect(Department.validate('CUSTOMER_service')).toBe(true);
+    expect(Department.validate('AdMinistrative')).toBe(true);
+    expect(Department.validate('Cleaning')).toBe(true);
+  })
+})
