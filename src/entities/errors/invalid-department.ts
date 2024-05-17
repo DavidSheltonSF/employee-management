feffaac8268e53f42f00b9ddbe9a35caa73ca320
DavidSheltonSF <david.faria012@gmail.com>
2024-05-16 22:53:24 -0300

    fix: Fixing name of InvalidDepartmentError error

--- src/entities/errors/invalid-department.ts
+++ src/entities/errors/invalid-department.ts
@@ -1,6 +1,6 @@
 import { DomainError } from "./domain-error";
 
-export class InvaliDepartmentError extends Error implements DomainError {
+export class InvalidDepartmentError extends Error implements DomainError {
   constructor(department: string){
     super(`The department "${department}" is invalid.`);
     this.name = 'InvalidDepartment';
