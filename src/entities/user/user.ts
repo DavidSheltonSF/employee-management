b97d627b289f4a5ffed257d70f4e77d0ca842e48
DavidSheltonSF <david.faria012@gmail.com>
2024-05-18 17:36:29 -0300

    feat(entities/user): Implementing User entity

---
+++ src/entities/user/user.ts
@@ -0,0 +1,64 @@
+import { UserData } from "./user-data";
+import { 
+  InvalidNameError, 
+  InvalidEmailError, 
+  InvalidPasswordError, 
+  InvalidUserRoleError } from "../errors";
+import { 
+  Name, Email, Password, UserRole } from "../validators";
+import { Either, left, right } from "../../shared/either";
+
+export class User {
+  private readonly name: Name;
+  private readonly email: Email;
+  private readonly userRole: UserRole
+  private readonly password: Password
+
+  private constructor(
+    name: Name, email: Email, 
+    userRole: UserRole, password: Password){
+    this.name = name;
+    this.email = email;
+    this.userRole = userRole;
+    this.password = password;
+  }
+
+  static create(userData: UserData){
+    const nameOrError: Either<InvalidNameError, Name> = Name.
+    create(userData.name);
+    const emailOrError: Either<InvalidEmailError, Email> = Email.
+    create(userData.email);
+    const userRoleOrError: Either<InvalidUserRoleError, UserRole> = UserRole.
+    create(userData.userRole);
+    const passwordOrError: Either<InvalidPasswordError, Password> = Password.
+    create(userData.password);
+
+    if(nameOrError.isLeft()){
+      return left(nameOrError.value);
+    }
+
+    if(emailOrError.isLeft()){
+      return left(emailOrError.value);
+    }
+
+    if(userRoleOrError.isLeft()){
+      return left(userRoleOrError.value);
+    }
+
+    if (passwordOrError.isLeft()){
+      return left(passwordOrError.value);
+    }
+
+    const name = nameOrError.value;
+    const email = emailOrError.value;
+    const userRole = userRoleOrError.value;
+    const password = passwordOrError.value;
+
+    return right(new User(
+      name,
+      email,
+      userRole,
+      password
+    ))
+  }
+}
