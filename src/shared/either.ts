07a566e9a1fac23f6b1346556feaaea3fb649a45
DavidSheltonSF <david.faria012@gmail.com>
2024-05-24 11:46:28 -0300

    fix(shared/eitherts): Replacing ifRight method name by isRight

--- src/shared/either.ts
+++ src/shared/either.ts
@@ -11,7 +11,7 @@ export class Left<L, A> {
     return true;
   }
 
-  ifRight(): this is Right<L, A> {
+  isRight(): this is Right<L, A> {
     return false;
   }
 }
@@ -27,7 +27,7 @@ export class Right<L, A> {
     return false;
   }
 
-  ifRight(): this is Right<L, A> {
+  isRight(): this is Right<L, A> {
     return true;
   }
 }
