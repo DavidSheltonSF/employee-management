f34905667e753dbd917fbd96c4bd67eb91bd6da2
DavidSheltonSF <david.faria012@gmail.com>
2024-05-24 14:25:02 -0300

    refactor(usecases/register-user): update register-user/response.ts

--- src/usecases/register-user/response.ts
+++ src/usecases/register-user/response.ts
@@ -1,7 +1,8 @@
 import { UserData } from "../../entities/user/user-data";
 import { InvalidEmailError, InvalidNameError, InvalidPasswordError, InvalidUserRoleError } from "../../entities/errors";
 import { Either } from "../../shared/either";
+import { UserExistsError } from "../errors/user-exists";
 
 
 export type RegisterUserResponse = Either<InvalidNameError | 
-InvalidEmailError | InvalidPasswordError | InvalidUserRoleError, UserData>
+InvalidEmailError | InvalidPasswordError | InvalidUserRoleError | UserExistsError, UserData>
