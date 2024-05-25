7101fdc76a09278b2ce807a279c757fea201ba38
DavidSheltonSF <david.faria012@gmail.com>
2024-05-25 18:24:59 -0300

    refactor: Updateing EmployeeRepository interface

--- src/usecases/ports/employee-repository.ts
+++ src/usecases/ports/employee-repository.ts
@@ -2,9 +2,8 @@ import { EmployeeData } from "../../entities/employee/employee-data"
 
 export interface EmployeeRepository {
   findAllEmployees: () => Promise<EmployeeData[]>
-  findEmployeeByEmail: (email: string) => Promise<EmployeeData>
+  findEmployeeByEmail: (email: string) => Promise<EmployeeData | null>
   add: (employeeData: EmployeeData) => Promise<void>
-  update: (employeeData: EmployeeData) => Promise<void>
-  dellete: (employeeData: EmployeeData) => Promise<void>
+  update: (employeeData: EmployeeData) => Promise<boolean>
   exists: (email: string) => Promise<boolean>
 }
