62fbbd453e80af9961314be9f6fbe30b196d4524
DavidSheltonSF <david.faria012@gmail.com>
2024-05-28 18:06:52 -0300

    refactor: Adding EmployeeDataWithout email to EmployeeRepository interface

--- src/usecases/_ports/employee-repository.ts
+++ src/usecases/_ports/employee-repository.ts
@@ -1,10 +1,11 @@
 import { EmployeeData } from "../../entities/employee/employee-data"
+import { EmployeeDataWithoutEmail } from "../alter-employee/interface"
 
 export interface EmployeeRepository {
   findAllEmployees: () => Promise<EmployeeData[]>
   findEmployeeByEmail: (email: string) => Promise<EmployeeData | null>
   add: (employeeData: EmployeeData) => Promise<void>
-  update: (email: string, employeeData: EmployeeData) => Promise<void>
+  update: (email: string, employeeData: EmployeeData | EmployeeDataWithoutEmail) => Promise<void>
   delete: (email: string)=> Promise<void>
   exists: (email: string) => Promise<boolean>
 }
