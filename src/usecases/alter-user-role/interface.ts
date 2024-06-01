c7997676909f349deb6147148de366430fb263e6
DavidSheltonSF <david.faria012@gmail.com>
2024-05-31 23:09:58 -0300

    refactor(usecases): Removing alter user role usecase

--- src/usecases/alter-user-role/interface.ts
+++
@@ -1,5 +0,0 @@
-import { AlterUserRoleResponse } from "./response";
-
-export interface AlterUserRoleInterface {
-  alterUserRole: (email: string, role: string) => Promise<AlterUserRoleResponse>
-}
--- src/usecases/alter-user-role/response.ts
+++
@@ -1,5 +0,0 @@
-import { Either } from "../../shared/either";
-import { NotAllowedError } from "../_errors/not-allowed";
-import { NoResultError } from "../_errors/no-result";
-
-export type AlterUserRoleResponse = Either<NotAllowedError | NoResultError, boolean>
