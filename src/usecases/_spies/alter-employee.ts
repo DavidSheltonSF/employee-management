6c509124cb4cd1d51e7f7d55fbdf2eb9ca49a869
DavidSheltonSF <david.faria012@gmail.com>
2024-06-02 21:11:43 -0300

    feat: Implementing AlterEmployee use case spy

---
+++ src/usecases/_spies/alter-employee.ts
@@ -0,0 +1,33 @@
+import { right, left } from "../../shared/either";
+import { EmployeeData } from "../../entities/employee/employee-data";
+import { EmployeeRepository } from "../_ports/employee-repository";
+import { AlterEmployeeInterface as AlterEmployee } from "../alter-employee/interface";
+import { AlterEmployeeResponse } from "../alter-employee/response";
+import { mock_employee } from "./helper/mock_employee";
+import { NoResultError } from "../_errors/no-result";
+
+export class AlterEmployeeSpy implements AlterEmployee {
+  private readonly employeeRepository: EmployeeRepository
+  alterParam: Record<string, EmployeeData> = {};
+
+  constructor(employeeRepo: EmployeeRepository){
+    this.employeeRepository = employeeRepo;
+  }
+
+  async alter(employeeData: EmployeeData): Promise<AlterEmployeeResponse> {
+
+    this.alterParam['employeeData'] = employeeData;
+
+    const { email } = employeeData;
+    const employee = await this.employeeRepository.findEmployeeByEmail(email);
+
+    if (!employee){
+      return left(new NoResultError(email))
+    }
+
+    await this.employeeRepository.update(employeeData);
+
+    return right(mock_employee());
+
+  }
+}
