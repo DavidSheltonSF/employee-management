1f1e15524877dbe440f5f90bab09821887e34f32
DavidSheltonSF <david.faria012@gmail.com>
2024-06-04 21:42:20 -0300

    feat(controllers): Implementing FindUserController

---
+++ src/adapters/presentation/controllers/find-user-controller.ts
@@ -0,0 +1,44 @@
+import { FindUserInterface as FindUser } from "../../../usecases/find-user/interface";
+import { badRequest, ok, unprocessableEntity, serverError } from "./_helpers/http-helper";
+import { HttpRequest, HttpResponse } from "./_ports/http";
+import { MissingParamError } from "./_errors/missing-param-error";
+import { FindUserResponse } from "../../../usecases/find-user/response";
+
+export class FindUserController {
+  private readonly findUser: FindUser
+
+  constructor(findUser: FindUser){
+    this.findUser = findUser
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse>{
+
+    try {
+
+      const { viewArgs } = httpRequest
+
+      if (viewArgs){
+        const { email } = viewArgs;
+
+        if (!email){
+          return badRequest(new MissingParamError('email'));
+        }
+
+        const response: FindUserResponse = await this.findUser.byEmail(email);
+
+        if(response.isLeft()){
+          return unprocessableEntity(response.value)
+        }
+
+        return ok(response);
+      }
+
+      const response: FindUserResponse = await this.findUser.all();
+      return ok(response);
+
+    }catch(err){
+      console.log(err);
+      return serverError('internal');
+    }
+  }
+}
