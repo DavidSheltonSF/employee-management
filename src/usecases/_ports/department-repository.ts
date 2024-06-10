5b2928c90a600f707e815eb01830cd14a59952f3
DavidSheltonSF <david.faria012@gmail.com>
2024-06-09 21:44:47 -0300

    feat(Department repository interface): Implementing findDepartmentByName function

--- src/usecases/_ports/department-repository.ts
+++ src/usecases/_ports/department-repository.ts
@@ -3,6 +3,7 @@ import { DepartmentData } from "../../entities/department/department-data";
 export interface DepartmentRepository {
   findAllDepartments: () => Promise<DepartmentData[]>
   findDepartmentByManagerEmail: (email: string) => Promise<DepartmentData | null>
+  findDepartmentByName: (name: string) => Promise<DepartmentData | null>
   add: (departmentData: DepartmentData) => Promise<void>
   update: (departmentData: DepartmentData | DepartmentData) => Promise<void>
   delete: (email: string) => Promise<void>
