83aa6dfda9f2492d4d125be1ec9d3f3dfaae3cf6
DavidSheltonSF <david.faria012@gmail.com>
2024-06-09 21:53:50 -0300

    refactor(RegisterDepartment use case): Implementing checking if manager email is registred if it was provided

--- src/usecases/register-department/register-department.ts
+++ src/usecases/register-department/register-department.ts
@@ -1,17 +1,22 @@
 import { InvalidNameError } from "../../entities/_errors";
 import { Department } from "../../entities/department/department";
 import { DepartmentData } from "../../entities/department/department-data";
+import { Employee } from "../../entities/employee/employee";
 import { Either, left, right } from "../../shared/either";
+import { ManagerNotFoundError } from "../_errors/ManagerNotFoundError";
 import { DuplicateDataError } from "../_errors/duplicate-data";
 import { DepartmentRepository } from "../_ports/department-repository";
+import { EmployeeRepository } from "../_ports/employee-repository";
 import { RegisterDepartmentInterface } from "./interface";
 import { RegisterDepartmentResponse } from "./response";
 
 export class RegisterDepartment implements RegisterDepartmentInterface {
   private readonly departmentRepository: DepartmentRepository;
+  private readonly employeeRepository: EmployeeRepository
 
-  constructor(departmentRepo: DepartmentRepository){
+  constructor(departmentRepo: DepartmentRepository, employeeRepo: EmployeeRepository){
     this.departmentRepository = departmentRepo;
+    this.employeeRepository = employeeRepo;
   }
 
   async register(departmentData: DepartmentData): Promise<RegisterDepartmentResponse> {
@@ -19,15 +24,27 @@ export class RegisterDepartment implements RegisterDepartmentInterface {
     const departmentOrError: Either<InvalidNameError, Department> = Department.create(departmentData);
 
     if (departmentOrError.isLeft()){
+      console.log('SOME ERROR')
       return left(departmentOrError.value)
     }
 
     const department = departmentOrError.value;
-    const exists = this.departmentRepository.exists(department.managerEmail.value);
+    const exists = this.departmentRepository.exists(department.name.value);
     if ((await exists).valueOf()){
-      return left(new DuplicateDataError(department.managerEmail.value))
+      console.log('DUPLICATED')
+      return left(new DuplicateDataError(department.name.value))
     }
 
+    if (department.managerEmail){
+      const managerExists = await this.employeeRepository.exists(department.managerEmail.value);
+      console.log(managerExists);
+      console.log(department)
+      if (!managerExists){
+        console.log('ManagerNotFound')
+        return left(new ManagerNotFoundError(department.managerEmail.value))
+      }
+    }
+    
     await this.departmentRepository.add(departmentData);
 
     return right(departmentData);
