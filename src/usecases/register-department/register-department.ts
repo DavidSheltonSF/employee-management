e67cf9adefa190dfe4a8d119717efbdb0f2c153f
DavidSheltonSF <david.faria012@gmail.com>
2024-06-13 19:31:56 -0300

    Update RegisterDepartment use case: Removing console.logs and simplifying the line that checks if a department exists in register() method

--- src/usecases/register-department/register-department.ts
+++ src/usecases/register-department/register-department.ts
@@ -1,7 +1,6 @@
 import { InvalidNameError } from "../../entities/_errors";
 import { Department } from "../../entities/department/department";
 import { DepartmentData } from "../../entities/department/department-data";
-import { Employee } from "../../entities/employee/employee";
 import { Either, left, right } from "../../shared/either";
 import { ManagerNotFoundError } from "../_errors/ManagerNotFoundError";
 import { DuplicateDataError } from "../_errors/duplicate-data";
@@ -24,23 +23,19 @@ export class RegisterDepartment implements RegisterDepartmentInterface {
     const departmentOrError: Either<InvalidNameError, Department> = Department.create(departmentData);
 
     if (departmentOrError.isLeft()){
-      console.log('SOME ERROR')
       return left(departmentOrError.value)
     }
 
     const department = departmentOrError.value;
-    const exists = this.departmentRepository.exists(department.name.value);
-    if ((await exists).valueOf()){
-      console.log('DUPLICATED')
+    const exists = await this.departmentRepository.exists(department.name.value);
+    if (exists){
       return left(new DuplicateDataError(department.name.value))
     }
 
     if (department.managerEmail){
       const managerExists = await this.employeeRepository.exists(department.managerEmail.value);
-      console.log(managerExists);
-      console.log(department)
+
       if (!managerExists){
-        console.log('ManagerNotFound')
         return left(new ManagerNotFoundError(department.managerEmail.value))
       }
     }
