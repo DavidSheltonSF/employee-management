a5c4f1843ad467d7ed1fd6713e6becd5832486e0
DavidSheltonSF <david.faria012@gmail.com>
2024-06-25 15:36:19 -0300

    test(Employee entity): Removing unused importation, Adding a new test to check if Entity will return an error when trying to create an enployee object without a name

--- src/entities/employee/employee.spec.ts
+++ src/entities/employee/employee.spec.ts
@@ -1,4 +1,3 @@
-import { Birthday, Department } from "../_validators";
 import { Employee } from "./employee";
 
 describe('Empoyee validator', () => {
@@ -17,5 +16,22 @@ describe('Empoyee validator', () => {
 
     expect(employeeOrError.isRight()).toBe(true);
 
-  })
+  });
+
+  test('Should return an error for an employee with invalid name', () => {
+    const employeeData = {
+      name: 'Fernando64',
+      lastName: 'Bento',
+      email: 'bento@bugmail.com',
+      birthday: '2002-05-20',
+      gender: 'male',
+      role: 'developer',
+      department: 'information_technology'
+    }
+    
+    const employeeOrError = Employee.create(employeeData)
+
+    expect(employeeOrError.isLeft()).toBe(true);
+
+  });
 })
