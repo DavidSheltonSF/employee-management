da51f7487e6206ca8a6ec505937d7223e50a5950
DavidSheltonSF <david.faria012@gmail.com>
2024-06-25 15:57:06 -0300

    refactor(usecase erros): Modifyng ManagerNotFoundError message

--- src/usecases/_errors/ManagerNotFoundError.ts
+++ src/usecases/_errors/ManagerNotFoundError.ts
@@ -2,7 +2,7 @@ import { UseCaseError } from "./interface/usecase-error";
 
 export class ManagerNotFoundError extends Error implements UseCaseError {
   constructor(email: string){
-    super(`The email "${email}" was not found.`)
+    super(`Manager email "${email}" was not found.`)
     this.name = 'ManagerNotFoundError'
   }
 }
