6cc4230a78747860b40c4f74ff6f05db33d3496e
DavidSheltonSF <david.faria012@gmail.com>
2024-06-24 23:25:53 -0300

    test(entities/_validators/tests/gender.spec.ts): Renaming a test to 'Should not accept valid gender if it is not lowercase'

--- src/entities/_validators/tests/gender.spec.ts
+++ src/entities/_validators/tests/gender.spec.ts
@@ -5,8 +5,8 @@ describe('Gender validator', () => {
     expect(Gender.validate('male')).toBe(true);
     expect(Gender.validate('female')).toBe(true);
   });
-
-  test('Should accept valid gender regardless if value is uppercase, lowercase, etc.', () => {
+  
+  test('Should not accept valid gender if it is not lowercase', () => {
     expect(Gender.validate('MALE')).toBe(true);
     expect(Gender.validate('FEMALE')).toBe(true);
     expect(Gender.validate('Male')).toBe(true);
@@ -14,14 +14,14 @@ describe('Gender validator', () => {
     expect(Gender.validate('MaLe')).toBe(true);
     expect(Gender.validate('FeMaLe')).toBe(true);
   })
-
+  
   test('Should not accept invalid gender value', () => {
     expect(Gender.validate('male4')).toBe(false);
     expect(Gender.validate('2female')).toBe(false);
     expect(Gender.validate('banana')).toBe(false);
 
   });
-
+  
   test('Should not accept void value', () => {
     expect(Gender.validate('')).toBe(false);
   });
