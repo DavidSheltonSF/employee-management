d37cbe7d4e5d0b27ba75b0c29bea0c4231486120
DavidSheltonSF <david.faria012@gmail.com>
2024-06-25 16:04:54 -0300

    refactor(usecase errors): Deleting NotAllowedError

--- src/usecases/_errors/not-allowed.ts
+++
@@ -1,8 +0,0 @@
-import { UseCaseError } from "./interface/usecase-error";
-
-export class NotAllowedError extends Error implements UseCaseError {
-  constructor(){
-    super(`You are not allowed to execute this action`);
-    this.name = 'notAllowedError';
-  }
-}
