b10bc7d1162f840bc29fe04b2b790f4806f91ae6
DavidSheltonSF <david.faria012@gmail.com>
2024-07-04 22:44:52 -0300

    test(AlterRole usecase)

---
+++ src/usecases/alter-role/alter-role.spec.ts
@@ -0,0 +1,53 @@
+import { AlterRole } from "./alter-role";
+import { SpyRoleRepository } from "../_in-memory-role-repository/spy-role-repository";
+import { left } from "../../shared/either";
+import { NoResultError } from "../_errors/no-result";
+
+
+const fakeDataBase = [
+  {
+    name: 'developer',
+    department: 'technology', 
+  },
+  {
+    name: 'manager',
+    department: 'administration', 
+  },
+]
+
+describe('alterRole validator', () => {
+
+  test('Should alter Role correctly', async () => {
+
+    const spyRoleRepository = new SpyRoleRepository(fakeDataBase);
+    const alterRoleUseCase = new AlterRole(spyRoleRepository);
+    
+    const response = await alterRoleUseCase.alter({
+      name: 'manager',
+      department: 'technology', 
+    });
+
+    // Checking if response of the usecase is right
+    expect(response.isRight()).toBeTruthy()
+
+    // Checking if params were added to the spy correctly
+    expect(spyRoleRepository.updateParams['roleData'])
+      .toEqual({
+        name: 'manager',
+        department: 'technology', 
+      });
+  });
+
+  test('Should return NoResultError for a not registered name', async () => {
+    const spyRoleRepository = new SpyRoleRepository(fakeDataBase);
+    const alterRoleUseCase = new AlterRole(spyRoleRepository);
+
+    const fakeRole = {
+      name: 'faken',
+      department: 'fekenaldo', 
+    }
+    const response = await alterRoleUseCase.alter(fakeRole);
+
+    expect(response).toEqual(left(new NoResultError(fakeRole.name)));
+  })
+})
