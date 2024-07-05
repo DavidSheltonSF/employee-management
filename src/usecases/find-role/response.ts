3f26d6880fef2532cee1c6f0b01aa51111166ee5
DavidSheltonSF <david.faria012@gmail.com>
2024-07-04 22:33:37 -0300

    fix(usecases/find-role): Removing InvalidEmailError typing and adding InvalidDepartmentError typing to Either typing of FindRoleResponse

--- src/usecases/find-role/response.ts
+++ src/usecases/find-role/response.ts
@@ -1,7 +1,7 @@
-import { InvalidEmailError } from "../../entities/_errors";
+import { InvalidDepartmentError } from "../../entities/_errors";
 import { NoResultError } from "../_errors/no-result";
 import { Either } from "../../shared/either";
 import { RoleData } from "../../entities/role/role-data";
 
-export type FindRoleResponse = Either<InvalidEmailError | 
+export type FindRoleResponse = Either<InvalidDepartmentError | 
 NoResultError, RoleData | RoleData[]>
